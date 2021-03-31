6525013af5c9d415d276f6c8d8b36e5c26688e05
anwj <anwj0601@163.com>
2021-03-31 14:54:07 +0800

    delete default config

--- java-sdk-samples/src/main/java/com/opay/java/sdk/samples/Config.java
+++ java-sdk-samples/src/main/java/com/opay/java/sdk/samples/Config.java
@@ -1,14 +1,8 @@
 package com.opay.java.sdk.samples;
 
-import java.util.Objects;
-
 public class Config {
 
-    public static final String MERCHANT_ID = "256621031818649";
-    public static final String PUBLIC_KEY = "OPAYPUB16160617080040.6961247863423267";
-    public static final String PRIVATE_KEY = "OPAYPRV16160617080050.388604510744898";
-
-    public static void main(String[] args) {
-        System.out.println(Objects.nonNull(null));
-    }
+    public static final String MERCHANT_ID = "YOU_MERCHANT_ID";
+    public static final String PUBLIC_KEY = "PUBLIC_KEY";
+    public static final String PRIVATE_KEY = "PRIVATE_KEY";
 }
